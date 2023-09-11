a46f5bb6fa48b3d20b5b2856650c131cb987ca08
praju-1 <prajaktatayde26@gmail.com>
2023-09-11 16:19:18 +0530

    File containing middleware for API's

---
+++ express.js
@@ -0,0 +1,29 @@
+// Importing require modules
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const router = require('./route');
+const env = require("dotenv");
+const postRoute = require("./post");
+
+// Creating Middleware for API 
+app.use(express.json());
+app.use("/user", router);
+app.use("/authorisedUser", postRoute);
+env.config()
+
+
+mongoose.set('strictQuery', false);
+//DB connection
+mongoose.connect(process.env.DB_connection)
+    // applying promise structure to handle data properly
+
+    .then(() => {
+        console.log("Connection successfull");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+// Port number to work code
+app.listen(4000);
