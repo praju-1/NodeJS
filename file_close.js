92525416d5056b692908d04ae1e8ca8ef82ef4ca
praju-1 <prajaktatayde26@gmail.com>
2023-09-15 14:38:47 +0530

    Closing operation on file

---
+++ file_close.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+
+
+
+// Open a file for reading
+fs.open('example.txt', 'r', (err, fd) => {
+    if (err) {
+        console.error('Error opening file:', err);
+        return;
+    }
+
+    // Do some operations with the file...
+
+    // Close the file when done
+    fs.close(fd, (err) => {
+        if (err) {
+            console.error('Error closing file:', err);
+            return;
+        }
+        console.log('File closed successfully.');
+    });
+});
