b1ef26066698868072150d219c59bf30fd48e91e
praju-1 <prajaktatayde26@gmail.com>
2023-09-16 14:25:14 +0530

    Updated

--- fs.js
+++ fs.js
@@ -2,14 +2,12 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 
-
-
-
 http.createServer((req, res) => {
     console.log("server is created");
 
     // read file
     fs.readFile("index.html", (err, data) => {
+        // defining header of http
         res.writeHead(200, { 'content-type': 'text/html' });
         res.write(data);
         return res.end();
