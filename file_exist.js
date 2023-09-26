89553b1233715d2aae80675dd8d080a2c87c4de3
praju-1 <prajaktatayde26@gmail.com>
2023-09-26 15:23:56 +0530

    Updated

--- file_exist.js
+++ file_exist.js
@@ -6,21 +6,20 @@ http.createServer((req, res) => {
     console.log("server Created");
 
 getCurrentFilenames();
-
+    // It checks the whether file is exist or not and return output in Boolean
     let fileExists = fs.existsSync('input.txt');
     console.log("input.txt exists:", fileExists);
 
     fileExists = fs.existsSync('file.txt');
     console.log("file.txt exists:", fileExists);
 
-    // Function to get current filenames
-    // in directory
+    // Function to get current filenames from directory
     function getCurrentFilenames() {
         console.log("\nCurrent filenames:");
+            // __dirname if global object
         fs.readdirSync(__dirname).forEach(file => {
             console.log(file);
         });
         console.log("\n");
     }
-
 }).listen(4000)
