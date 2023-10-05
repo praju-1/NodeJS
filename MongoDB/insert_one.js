99fe199248f8062f095aacea3e6dd25b91fd9d26
praju-1 <prajaktatayde26@gmail.com>
2023-10-05 17:31:36 +0530

    Adding one record into database

---
+++ MongoDB/insert_one.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://prajakta:praju%40123@cluster0.jz314qu.mongodb.net/test';
+
+// Create a MongoClient and connect to the MongoDB server
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+
+        const db = client.db(); // The database name can be specified here if needed
+
+        // Define the object to insert
+        const myobj = { name: "Ajeet Kumar", age: "28", address: "Delhi" };
+
+        // Insert the object into a collection
+        const result = await db.collection('myDatabase').insertOne(myobj);
+
+        console.log(`Inserted ${result.insertedCount} record with _id: ${result.insertedId}`);
+
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        // Close the MongoClient
+        client.close();
+    }
+}
+// Call the run function to execute the code
+run();
+
