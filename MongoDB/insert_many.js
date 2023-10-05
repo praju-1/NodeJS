eda2e8e9d400118183c76c485d86d20e7db55cd7
praju-1 <prajaktatayde26@gmail.com>
2023-10-05 17:42:37 +0530

    Updated with _id field

--- MongoDB/insert_many.js
+++ MongoDB/insert_many.js
@@ -13,14 +13,21 @@ async function run() {
         const db = client.db(); // The database name can be specified here if needed
 
         // Define the object to insert
-        const myobj = [
-            { name: 'John', age: "23", address: 'Highway 71'},
-            { name: 'Peter', age: "29", address: 'Lowstreet 4'},
-            { name: 'Amy', age: "28",  address: 'Apple st 652'},
-            { name: 'Hannah', age: "27",  address: 'Mountain 21'},
-            { name: 'Michael', age: "22",  address: 'Valley 345'},
-            { name: 'Sandy', age: "22",  address: 'Ocean blvd 2'},
-            { name: 'Betty',age: "30",  address: 'Green Grass 1'}];
+
+        // adding _id field
+        const myobj = [{ _id: 154, name: 'Chocolate Heaven' },
+        { _id: 155, name: 'Tasty Lemon' },
+        { _id: 156, name: 'Vanilla Dream' }]
+
+
+        // without _id field which generate default unique id for each record
+        // { name: 'John', age: "23", address: 'Highway 71'},
+        // { name: 'Peter', age: "29", address: 'Lowstreet 4'},
+        // { name: 'Amy', age: "28",  address: 'Apple st 652'},
+        // { name: 'Hannah', age: "27",  address: 'Mountain 21'},
+        // { name: 'Michael', age: "22",  address: 'Valley 345'},
+        // { name: 'Sandy', age: "22",  address: 'Ocean blvd 2'},
+        // { name: 'Betty',age: "30",  address: 'Green Grass 1'}];
 
         // Insert the object into a collection
         const result = await db.collection('myDatabase').insertMany(myobj);
