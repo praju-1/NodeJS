233123df2d8ab75dce8bbd289ec071dd9943c965
praju-1 <prajaktatayde26@gmail.com>
2023-10-05 17:32:11 +0530

    Added multiple record in database

---
+++ MongoDB/insert_many.js
@@ -0,0 +1,39 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://prajakta:praju%40123@cluster0.jz314qu.mongodb.net/test';
+
+// Create a MongoClient and connect to the MongoDB server
+const client = new MongoClient(url);
+
+async function run() {
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+
+        const db = client.db(); // The database name can be specified here if needed
+
+        // Define the object to insert
+        const myobj = [
+            { name: 'John', age: "23", address: 'Highway 71'},
+            { name: 'Peter', age: "29", address: 'Lowstreet 4'},
+            { name: 'Amy', age: "28",  address: 'Apple st 652'},
+            { name: 'Hannah', age: "27",  address: 'Mountain 21'},
+            { name: 'Michael', age: "22",  address: 'Valley 345'},
+            { name: 'Sandy', age: "22",  address: 'Ocean blvd 2'},
+            { name: 'Betty',age: "30",  address: 'Green Grass 1'}];
+
+        // Insert the object into a collection
+        const result = await db.collection('myDatabase').insertMany(myobj);
+
+        console.log("Many document inserted");
+
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        // Close the MongoClient
+        client.close();
+    }
+}
+// Call the run function to execute the code
+run();
+
