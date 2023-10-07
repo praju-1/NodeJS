cc8465ca1f847775714ea6375bec6adde887a2ab
praju-1 <prajaktatayde26@gmail.com>
2023-10-07 17:30:46 +0530

    Data find through query

---
+++ MongoDB/query.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb');
+const env = require("dotenv");
+env.config();
+
+// Create a MongoClient and connect to the MongoDB server
+const client = new MongoClient(process.env.DB_connection);
+
+async function run() {
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+
+        const db = client.db(); // The database name can be specified here if needed
+
+        // Find some data 
+        // var query = { address: "Green Grass 1"};
+
+        var query = { address: /^D/}
+        const result = await db.collection('myDatabase').find(query).toArray()
+
+        console.log(result);
+
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        // Close the MongoClient
+        client.close();
+    }
+}
+// Call the run function to execute the code
+run();
+
