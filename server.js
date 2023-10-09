48ee9bc0900f75b450e47a2deef7fea9d557c533
praju-1 <prajaktatayde26@gmail.com>
2023-10-09 11:24:13 +0530

    updated

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const http = require('http');
 const uc = require('uppercase')
 
+// Creating our own server
 http.createServer((req, res) => {
     // res.write(uc("Hello World..!"))
     // res.end()
+    
+
+    // meta for output
     res.writeHead(200, {'content-type' : 'text/html'});
 
     res.end(uc('<h3>Hello world..!</h3>'))
+ 
 }).listen(5000);
  
