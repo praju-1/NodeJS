0ddd9c60958bd8ef176d927fc37444fec281a610
praju-1 <prajaktatayde26@gmail.com>
2024-05-31 12:18:22 +0530

    Add env variable

--- MongoDB/create_database.js
+++ MongoDB/create_database.js
@@ -1,35 +1,14 @@
-// Import the mongoose library
-const mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+const env = require("dotenv");
+env.config();
 
-// MongoDB connection URL
-const mongoURL = 'mongodb+srv://prajakta:praju%40123@cluster0.jz314qu.mongodb.net/'; //Replace with your MongoDB URL
-
-mongoose.set('strictQuery', false);
-// Create a new Mongoose connection
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Bind connection to error event (to get notifications of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Bind connection to open event (to perform operations after the connection is successfully opened)
-db.once('open', () => {
-    console.log('Connected to MongoDB!');
-
-    // Create a new database using Mongoose
-    db.createCollection('myDatabase', (err, result) => {
-        if (err) {
-            console.error('Error creating database:', err);
-        } else {
-            console.log('Database "myDatabase" created successfully!');
-        }
-
-        // Close the MongoDB connection (optional)
-        mongoose.connection.close();
+MongoClient.connect(process.env.DB_connection, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    //Create a collection name "customers":
+    dbo.createCollection("customers", function (err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        db.close();
     });
 });
